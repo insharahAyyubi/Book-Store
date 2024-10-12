83d2f3c3051711acdcc4dc7246836fdccc3fd97b
Insharah Ayyubi <weharrypotter@gmail.com>
2024-10-13 01:54:34 +0530

    build changes

--- Backend/index.js
+++ Backend/index.js
@@ -27,10 +27,10 @@ mongoose
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.use("/book", bookRoute);
