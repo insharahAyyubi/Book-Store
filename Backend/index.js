80f110960f9dd43781a88a9544ba1958e76c3ea6
Insharah Ayyubi <weharrypotter@gmail.com>
2024-10-12 12:02:18 +0530

    express port changes

--- Backend/index.js
+++ Backend/index.js
@@ -29,6 +29,6 @@ app.get("/", (req, res) => {
   res.send("bella!");
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`);
 });
