9c33dd058ff13c915c9e98bcfd063b46499afa1f
Insharah Ayyubi <weharrypotter@gmail.com>
2024-10-13 01:25:42 +0530

    authContext changes

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -1,12 +1,21 @@
-import React, { createContext } from "react";
-import { useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 export const AuthContext = createContext();
+
 export default function AuthProvider({ children }) {
   const initialUser = localStorage.getItem("Users");
   const [authUser, setAuthUser] = useState(
     initialUser ? JSON.parse(initialUser) : undefined
   );
+
+  // Update local storage whenever authUser changes
+  useEffect(() => {
+    if (authUser) {
+      localStorage.setItem("Users", JSON.stringify(authUser));
+    } else {
+      localStorage.removeItem("Users");
+    }
+  }, [authUser]);
 
   return (
     <AuthContext.Provider value={[authUser, setAuthUser]}>
