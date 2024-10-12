aa55e6d084d9bc190f0d695dfc268f8cebc2d1a0
Insharah Ayyubi <103804673+insharahAyyubi@users.noreply.github.com>
2024-10-13 00:23:56 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -8,7 +8,7 @@ const Course = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://book-store-backend-wj1e.onrender.com/book");
         setBook(res.data);
       } catch (error) {
         console.log(error);
