c354b4350b50a1f0a73c304e9d151cc4569cf873
Insharah Ayyubi <weharrypotter@gmail.com>
2024-10-13 12:26:53 +0530

    backend done

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,6 @@ import mongoose from "mongoose";
 import bookRoute from "./route/book_route.js";
 import userRoute from "./route/user_route.js";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 app.use(cors());
@@ -14,7 +12,6 @@ app.use(express.json());
 const port = process.env.port || 4000;
 const URI = process.env.mongoDB_URI;
 
-// MongoDB connection
 mongoose
   .connect(URI)
   .then(() => {
@@ -24,26 +21,9 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
-// API Routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
-// Serve static files from the /dist directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(__filename);
-console.log(__dirname);
-
-app.use(express.static(path.join(__dirname, '../Frontend/dist'))); // Adjust path as needed
-
-// Catch-all route to serve index.html for React Router
-app.get('*', (req, res) => {
-  const fullpath = path.join(__dirname, '../Frontend/dist', 'index.html');
-  console.log(fullpath);
-  res.sendFile(fullpath);
-});
-
-// Start the server
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
 });
